f9ae4a6396b02e46a79fe3dc421b06a3e072844f
abdelrhmanfahmi <abdo.fahmy9696@gmail.com>
2020-12-01 14:30:57 +0200

    fahmy9

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @component('components.app')
 <x-header />
 <h1>ay betngan</h1>
-zero web school
+<h1>zero web school</h1>
 <section class="sptb">
     <div class="container customerpage">
         <div class="row">
